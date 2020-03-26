3f37885026dd226940ab0cc764d6868259e1a0b8
Sean Gillespie <sean@swgillespie.me>
2020-03-26 01:07:13 -0700

    Prep compiler

--- src/compiler.c
+++ src/compiler.c
@@ -2,13 +2,34 @@
 #include "value.h"
 #include "vm.h"
 
+// https://github.com/bazelbuild/starlark/blob/master/spec.md#lexical-elements
+// https://docs.python.org/3/reference/lexical_analysis.html
+typedef enum token_type
+{
+  TOKEN_IDENTIFIER,
+  TOKEN_NUMBER,
+  TOKEN_EQUALS,
+  TOKEN_EOF
+} token_type;
+
+typedef struct token
+{
+  token_type type;
+  int length;
+  int line;
+} token;
+
+typedef struct parser
+{
+  const char* source;
+} parser;
+
 struct compiler
 {
   starlark_thread_t* thread;
   object_module* module;
   object_function* func;
   bytecode_builder builder;
-  const char* source;
 };
 
 static object_function*
@@ -36,11 +57,11 @@ starlark_compile(starlark_thread_t* thread,
                  object_module* module,
                  const char* source)
 {
+  UNUSED_PARAMETER(source);
   thread->compiler = malloc(sizeof(compiler));
   INVARIANT(thread->compiler != NULL);
   thread->compiler->thread = thread;
   thread->compiler->module = module;
-  thread->compiler->source = source;
   object_function* fn = compile(thread->compiler);
   free(thread->compiler);
   thread->compiler = NULL;
